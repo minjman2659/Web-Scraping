846de22980e037d51744e7d2aef76a4664536016
minjman2659 <minjman2659@gmail.com>
2022-03-11 17:56:26 +0900

    Feat: [Html] index.html 생성

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-const { missingPath, errorHandler } = require('middleware');
-const routes = require('./routes');
+const { missingPath, errorHandler } = require("middleware");
+const { imagesDir } = require("lib");
+const routes = require("./routes");
 
+const template = path.join(__dirname, "../public", "index.html");
 const app = express();
 
 app.use(cookieParser());
-app.use(express.json({ limit: '30mb' }));
+app.use(express.json({ limit: "30mb" }));
 
-app.use('/', routes);
+app.get("/", (req, res) => {
+  res.sendFile(template);
+});
+app.use("/", routes);
 app.use(missingPath);
 app.use(errorHandler);
 
