4bba808924c525c1c2bc0121cf25f4b380966221
minjman2659 <minjman2659@gmail.com>
2022-03-24 21:07:13 +0900

    Refact: 코드 리팩토링

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const app = require('app');
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is running, port number is ${PORT} `);
+  console.log(`Server is running, port number is ${PORT}`);
 });
