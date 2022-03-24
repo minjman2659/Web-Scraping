dafa5162f012b2d253c7161d1e1b7cf06a2f8ab1
minjman2659 <minjman2659@gmail.com>
2022-03-24 21:03:35 +0900

    Fix: env 처리

--- src/lib/get-news-from-openAPI.js
+++ src/lib/get-news-from-openAPI.js
@@ -4,16 +4,16 @@ const axios = require('axios');
 
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
-const { CLIENT_ID, CLIENT_SECRET } = process.env;
-
-if (!CLIENT_ID || !CLIENT_SECRET) {
-  throw new Error('NO_ENV');
-}
-
 // 참고) 네이버 오픈API
 // 호출제한: 25,000회/일
 // https://developers.naver.com/docs/serviceapi/search/news/news.md#%EB%89%B4%EC%8A%A4
 const getNewsFromOpenAPI = async (queryUrl) => {
+  const { CLIENT_ID, CLIENT_SECRET } = process.env;
+
+  if (!CLIENT_ID || !CLIENT_SECRET) {
+    throw new Error('NO_ENV');
+  }
+
   const url = `https://openapi.naver.com/v1/search/news.json?${queryUrl}`;
 
   try {
